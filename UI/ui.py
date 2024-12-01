eb81869a571f9d08dbc6ad7631ca85751a52de01
RimWw <wilbertwu55@gmail.com>
2024-12-01 15:31:48 +0800

    Finished model for now

--- UI/ui.py
+++ UI/ui.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import model
 
 app = Flask(__name__)
 
 # Configure upload folder and allowed extensions
 UPLOAD_FOLDER = 'static/uploads'
+SAVE_FOLDER = 'static/saves'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SAVE_FOLDER'] = SAVE_FOLDER
 
 # Ensure the upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -26,14 +29,22 @@ def home():
         file = request.files['file']
         if file and allowed_file(file.filename):
             # Save the file
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], "Data.jpg")
+            savepath = os.path.join(app.config['SAVE_FOLDER'], "Output.jpg")
+            outputpath = savepath
             file.save(filepath)
-            print(filepath)
+
+            result = model.test_model(filepath,savepath)
+            if result == "Pneumonia":
+                outputpath = savepath
+            else:
+                outputpath = filepath
+
             # Pass the file path to render_template to display the image
-            return render_template('index.html', uploaded_file=filepath)
+            return render_template('index.html', uploaded_file=outputpath, label=result)
     
     # Render the page without an uploaded file
-    return render_template('index.html', uploaded_file=None)
+    return render_template('index.html', uploaded_file=None, label = "No Output")
 
 if __name__ == '__main__':
     app.run(debug=True)
